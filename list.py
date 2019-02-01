a0cb314409c511a9c2852666d72b8c1102ab47d5
Yugandran1812 <46665588+Yugandran1812@users.noreply.github.com>
2019-02-01 13:11:39 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,9 @@
+value=[]
+value.extend([11,11,2,22,23,3,2])#add more than a value in list
+value.append(55)#add single value
+print(value)
+print(value.count(11))#count no.of given value
+value.insert(3,12)#insert value @ given position (position,value)
+print(value)
+value[2]=99#replace the value 
+print(value)
